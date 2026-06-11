6bf379f02ad3aec9223e6b5cda1aadce49b3cdcc
KarlBelousov <belousovkarl@gmail.com>
2026-06-11 22:57:08 +0600

    заполнил базу данных данными

--- src/lib/placeholder-data.ts
+++ src/lib/placeholder-data.ts
@@ -25,17 +25,17 @@ const categories = [
     character_code: "boots",
   },
   {
-    id: "u6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
     category_name: "Одежда",
     character_code: "clothing",
   },
   {
-    id: "d6h15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    id: "f49d8193-7b4c-4786-a702-9972e573bda9",
     category_name: "Инструменты",
     character_code: "tools",
   },
   {
-    id: "d6e15727-9fe1-4981-8c5b-ea44a9bd81aa",
+    id: "f31f0c28-1ef9-4e6b-8e47-a9539a987e38",
     category_name: "Разное",
     character_code: "other",
   },
@@ -43,7 +43,7 @@ const categories = [
 
 const lots = [
   {
-    id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    id: "c49784f1-0b6e-4656-a35d-1b53c57a9142",
     title: "2014 Rossignol District Snowboard",
     lot_description:
       "Легкий маневренный сноуборд, готовый дать жару в любом парке",
@@ -55,7 +55,7 @@ const lots = [
     user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
   },
   {
-    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    id: "c4ca49b9-9301-4246-81bd-478c13bcc0db",
     title: "DC Ply Mens 2016/2017 Snowboard",
     lot_description:
       "Легкий маневренный сноуборд, готовый дать жару в любом парке",
@@ -67,7 +67,7 @@ const lots = [
     user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
   },
   {
-    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    id: "a2a85f64-a08f-4e56-a36a-cd120e3a7e28",
     title: "Крепления Union Contact Pro 2015 года размер L/XL",
     lot_description: "Хорошие крепления, надежные и легкие",
     img_url: "img/lot-3.jpg",
@@ -79,4 +79,13 @@ const lots = [
   },
 ];
 
-export { categories, lots, users };
+const bets = [
+  {
+    id: "f795cb9b-c14f-45ea-b392-e4899e595672",
+    price_bet: 5400,
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    lot_id: "c4ca49b9-9301-4246-81bd-478c13bcc0db",
+  },
+];
+
+export { categories, lots, users, bets};
